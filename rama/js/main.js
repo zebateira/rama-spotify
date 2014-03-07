7ff144fe6a4605f3f0f5abbb7fe99c874d75503f
Zé Bateira <jlageb+git@gmail.com>
2014-03-07 16:39:38 +0000

    graph drawing with depth = 1. using visjs

--- rama/js/main.js
+++ rama/js/main.js
@@ -1,25 +1,26 @@
 spotify.require([
-    '$api/models',
-    'js/now',
-    'js/top',
-    'js/search'
-    ], function(models, now, top, search) {
-        
-    models.application.load('arguments').done(tabs);
+  '$api/models',
+  'js/now',
+  'js/top',
+  'js/search'
+], function(models, now, top, search) {
 
-    models.application.addEventListener('arguments', tabs);
+  models.application.load('arguments').done(tabs);
 
-    function tabs() {
-        var args = models.application.arguments;
-        var current = document.getElementById(args[0]);
-        var sections = document.getElementsByClassName('section');
-        for (var i = 0, l = sections.length; i < l; i++){
-            sections[i].style.display = 'none';
-        }
-        current.style.display = 'block';
+  models.application.addEventListener('arguments', tabs);
+
+  function tabs() {
+    var args = models.application.arguments;
+    var current = document.getElementById(args[0]);
+    var sections = document.getElementsByClassName('section');
+    for (var i = 0, l = sections.length; i < l; i++) {
+      sections[i].style.display = 'none';
     }
+    current.style.display = 'block';
+  }
 
-    new now.NowPlaying().load();
-    new top.TopList().load();
-    new search.Search().load();
+
+  new now.NowPlaying().load();
+  new top.TopList().load();
+  new search.Search().load();
 });
--- rama/js/now.js
+++ rama/js/now.js
@@ -2,10 +2,10 @@ require(['$api/models', '$views/image'], function(models, image) {
 
   exports.NowPlaying = function() {
 
-    this.load = function() {
+    this.grid = function() {
 
       models.player.load('track').done(function(player) {
-       var currentArtist = models.Artist.fromURI(player.track.artists[0].uri);
+        var currentArtist = models.Artist.fromURI(player.track.artists[0].uri);
 
         currentArtist.load('related').done(function(artist) {
           artist.related.snapshot().done(function(snapshot) {
@@ -14,7 +14,12 @@ require(['$api/models', '$views/image'], function(models, image) {
 
             artists.forEach(function(artist) {
 
-              var artistCover = image.Image.forArtist(artist, {width: 100, height: 100, player: true, playerCentered: true});
+              var artistCover = image.Image.forArtist(artist, {
+                width: 100,
+                height: 100,
+                player: true,
+                playerCentered: true
+              });
 
               $('.now-playing').append($(artistCover.node).addClass('artist-cover'));
               artistCover.setStyle('plain');
@@ -23,5 +28,55 @@ require(['$api/models', '$views/image'], function(models, image) {
         });
       });
     };
+
+    this.drawGraph = function() {
+
+      models.player.load('track').done(function(player) {
+        var currentArtist = models.Artist.fromURI(player.track.artists[0].uri);
+        var index = 1;
+        var nodes = [{
+          id: index,
+          label: currentArtist.name
+        }];
+        var edges = [];
+
+        var container = document.getElementById('graph');
+        var data = {
+          nodes: nodes,
+          edges: edges
+        };
+
+        var options = {};
+
+        var graph = new vis.Graph(container, data, options);
+
+        currentArtist.load('related').done(function(artist) {
+          artist.related.snapshot().done(function(snapshot) {
+
+            snapshot.loadAll('name').each(function(artist) {
+              data.nodes.push({
+                id: ++index,
+                label: artist.name
+              });
+
+              data.edges.push({
+                from: 1,
+                to: index
+              });
+
+              graph.setData(data);
+            });
+
+          });
+
+        });
+      });
+
+    };
+
+    this.load = function() {
+      this.drawGraph();
+    };
+
   };
 });
