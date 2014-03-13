f90837c9a96de990457bd807c43870ce8967cade
Zé Bateira <jlageb+git@gmail.com>
2014-03-13 18:25:26 +0000

    travis ci for jasmine-jquery

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js', 'js/**/*.js']
+      all: ['Gruntfile.js', 'js/**/*.js', 'test/*.js']
     },
     jasmine: {
       test: {
