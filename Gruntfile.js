7cb32f4958fb6b4105ca7219462f1d8883cb4a0f
Zé Bateira <jlageb+git@gmail.com>
2014-03-14 18:52:06 +0000

    build stuff added. still not working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,42 @@ module.exports = function(grunt) {
     watch: {
       files: ['Gruntfile.js', 'js/*.js', 'sass/*.scss'],
       tasks: ['jshint', 'compass', 'jasmine']
-    }
+    },
+    cssmin: {
+      compress: {
+        files: {
+          'css/main.min.css': ['css/main.css']
+        }
+      }
+    },
+    useminPrepare: {
+      options: {
+        dest: 'dist'
+      },
+      html: {
+        src: ['index.html', 'views/*.html']
+      },
+      css: {
+        src: 'css/main.min.css'
+      }
+    },
+    usemin: {
+      options: {
+        dirs: ['dist']
+      },
+      html: ['dist/{,*/}*.html'],
+      css: ['dist/css/{,*/}*.css']
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  grunt.registerTask('build', ['useminPrepare', 'usemin', 'compass', 'cssmin', 'concat', 'uglify']);
 };
