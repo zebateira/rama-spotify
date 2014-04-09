ff49d2d7f9d62cceaeea85e3c43a73b5863f86ff
Zé Bateira <jlageb+git@gmail.com>
2014-04-09 16:56:57 +0100

    deploy script added

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+version="v0.1.3"
+
 mkdir ~/Spotify ; cd ~/Spotify
-wget https://github.com/carsy/rama-spotify/releases/download/v0.1.2/rama-spotify_v0.1.2.tar.gz
-tar -xvf rama-spotify_v0.1.2.tar.gz
+wget https://github.com/carsy/rama-spotify/releases/download/"$version"/rama-spotify_"$version".tar.gz
+tar -xvf rama-spotify_"$version".tar.gz
 open spotify:app:rama-spotify
