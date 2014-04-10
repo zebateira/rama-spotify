85247e5cb559aa600628bd9782bf345851e01271
Zé Bateira <jlageb+github@gmail.com>
2014-04-10 16:47:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 mkdir ~/Spotify ; cd ~/Spotify
 rm -rf rama-spotify
-wget https://github.com/carsy/rama-spotify/releases/download/v0.1.2/rama-spotify_v0.1.2.tar.gz
-tar -xvf rama-spotify_v0.1.2.tar.gz
+wget https://github.com/carsy/rama-spotify/releases/download/v0.1.3/rama-spotify_v0.1.3.tar.gz
+tar -xvf rama-spotify_v0.1.3.tar.gz
 open spotify:app:rama-spotify
