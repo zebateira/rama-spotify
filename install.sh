31e54b1a860b23e5c88933789543fa3c9910ceee
Zé Bateira <jlageb+git@gmail.com>
2014-04-11 18:18:19 +0100

    release v0.2

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="v0.1.3"
+version="v0.2"
 
 mkdir ~/Spotify ; cd ~/Spotify
 wget https://github.com/carsy/rama-spotify/releases/download/"$version"/rama-spotify_"$version".tar.gz
