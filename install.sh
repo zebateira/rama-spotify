e42e62d9844a503904cd7391cad43fe02ad5e121
Zé Bateira <jlageb+git@gmail.com>
2014-05-05 14:55:07 +0100

    install script: curl not saving file

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ rm -rf rama-spotify
 rm -rf rama
 rm rama_"$version".tar.gz
 rm rama_"$version".zip
-curl https://github.com/carsy/rama-spotify/releases/download/"$version"/rama_"$version".tar.gz
+curl -O https://github.com/carsy/rama-spotify/releases/download/"$version"/rama_"$version".tar.gz
 tar -xvf rama_"$version".tar.gz
 open spotify:app:rama
