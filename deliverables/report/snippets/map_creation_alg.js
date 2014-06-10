288a633ad59d8f39dd797b11d5e98a67d7655e3b
Zé Bateira <jlageb+git@gmail.com>
2014-06-10 19:12:09 +0100

    chap4 updated: new report structure.

--- deliverables/report/snippets/map_creation_alg.js
+++ deliverables/report/snippets/map_creation_alg.js
@@ -17,16 +17,15 @@ function expandNode(depth, parentArtist) {
   // after expanding, the node will stop being a leaf
   node.isLeaf = false;
 
-  // retrieve this.branching number of childs
+  // retrieve this.branching number of childs of the parent artist
   var relatedArtists = parentArtist.getRelatedArtists(this.branching);
 
+  // for each child artist, insert and create a node into the graph
+  // and do the recursive call for the child, but with decreased depth.
   for (var childArtist in relatedArtists) {
     this.insertNode(childArtist);
-    this.insertEdge({
-      from: parentArtist,
-      to: childArtist
-    });
 
+    // note that the stop condition of the recursion is depth <= 0
     if (depth > 0)
       expandNode(depth - 1, childArtist);
   }
